79c8de262defb08db2d3f61374da7176cafaef92
Денис Черногоров <chernogorov_ds@outlook.com>
2022-07-08 16:43:27 +0300

    added assert to test

--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -7,8 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 class TestMultiLang():
     def test_contains_bucket_button(self, browser):
         browser.get(link)
-        try:
-            browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-        except NoSuchElementException:
-            return False
-        return True
+        button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+        assert button, "Button is not contains on the page"
